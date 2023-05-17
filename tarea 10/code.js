82653ab507703893cfc5f6cc15784d35ebe0bf22
AMAYADIEGO <132412290+AMAYADIEGO@users.noreply.github.com>
2023-05-17 17:23:59 -0400

    Add files via upload

---
+++ tarea 10/code.js
@@ -0,0 +1,28 @@
+function mayorAByC(){
+   var a = parseInt(prompt("Ingrese A",8))
+
+   var b = parseInt(prompt("Ingrese B",4))
+    
+    var c = parseInt(prompt("Ingrese C",7))
+    
+        if (a > b ){
+    
+            if (a > c){
+    
+                alert("El numero mayor es A :" + a)
+    
+           } else {
+    
+                alert("El numero mayor es C : " + c)
+    
+    
+            if (b > c){
+    
+                alert("EL numero mayor es B : " + b)
+    
+            }else
+    
+                alert("El numro mayor es C : " + c)
+        }    
+    }   
+}      
